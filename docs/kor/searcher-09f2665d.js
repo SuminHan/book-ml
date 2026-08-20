e3e1738776de79233646bbbbc93ce8ed4baba8f2
Sumin Han <suminkaist@gmail.com>
2026-08-20 23:35:02 +0900

    ML1 Ch3: expand 3.1/3.2/3.3 to ch01-level density (worked examples, code verification, common mistakes, FAQ)

--- docs/kor/searcher-09f2665d.js
+++ docs/kor/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-668984bf.js',
+                path_to_root + 'searchindex-b56ddc89.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
